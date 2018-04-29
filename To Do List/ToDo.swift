ed5c6007523194db3979040b643fca205025cb0a
Carmelle Jean-Paul <cajeanpaul@gmail.com>
2018-04-28 20:43:36 -0400

    Just made my To Do Lost by inputting data into the code. This was very complicated so I may have to review notes. Now we are going to learn how to input data directly using the app using the plus button like a real user, not developer.

---
+++ To Do List/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  To Do List
+//
+//  Created by Carmelle Jean-Paul on 4/28/18.
+//  Copyright © 2018 Carmelle Jean-Paul. All rights reserved.
+//
+
+import UIKit
+
+
+class ToDo {
+    var name = ""
+    var important = false
+}
