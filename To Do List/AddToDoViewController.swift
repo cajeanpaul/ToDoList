35cd630268fc9862e473ae7247595216436eb245
Carmelle Jean-Paul <cajeanpaul@gmail.com>
2018-04-29 21:32:13 -0400

    Just finished connecting the app and story board. About to enter Class 6 where we will improve on the adding to do's functionality

---
+++ To Do List/AddToDoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddToDoViewController.swift
+//  To Do List
+//
+//  Created by Carmelle Jean-Paul on 4/29/18.
+//  Copyright © 2018 Carmelle Jean-Paul. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    @IBOutlet weak var taskTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
